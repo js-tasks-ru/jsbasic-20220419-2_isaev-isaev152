11d6a267e2a827f524cd87dad5755bab4d759c2b
Isaev Sergey <86910580+Sergey1522@users.noreply.github.com>
2022-04-20 15:25:51 +0300

    factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+
+  for (let res = 1; n > 1; n--) {
+   
+    res *= n;
+  //   console.log(res);
+  
+   }
+
 }
+factorial(0);
+factorial(1);
+factorial(3);
+factorial(5);
