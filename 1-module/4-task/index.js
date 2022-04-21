9e9ac03d972dd4f506ea0319558a8f54f17c91a3
Isaev Sergey <86910580+Sergey1522@users.noreply.github.com>
2022-04-21 16:49:04 +0300

    Функция заглавная буква

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,13 @@
 function checkSpam(str) {
   // ваш код...
+   str = str.toUpperCase();
+  
+
+  if (str.includes('XXX') || str.includes('1XBET') === true) {
+    console.log(str);
+
+    return true;
+ 
+  }return false;
 }
+checkSpam(str);
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,6 @@
 function truncate(str, maxlength) {
   // ваш код...
+  return str.length <= strLength ? str : str.slice(0, strLength) + "...";
 }
+trunc('Вот, что мне хотелось бы сказать на эту тему:', 20);
+trunc('Всем привет!', 20);
