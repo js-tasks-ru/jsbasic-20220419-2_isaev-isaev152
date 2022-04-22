5deb5da967e5023e41ba25e682c60a50a0cf1692
Isaev Sergey <86910580+Sergey1522@users.noreply.github.com>
2022-04-22 07:43:55 +0300

    функция валидации

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -2,5 +2,5 @@ function truncate(str, maxlength) {
   // ваш код...
   return str.length <= maxlength ? str : str.slice(0, maxlength) + "...";
 }
-trunc('Вот, что мне хотелось бы сказать на эту тему:', 20);
-trunc('Всем привет!', 20);
+truncate('Вот, что мне хотелось бы сказать на эту тему:', 20);
+truncate('Всем привет!', 20);
