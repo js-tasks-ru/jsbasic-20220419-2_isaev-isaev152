73586430494b5fa215046140352da4af639b9a4c
Isaev Sergey <86910580+Sergey1522@users.noreply.github.com>
2022-04-24 08:12:17 +0300

    выполнение 1 задания

---
+++ 1-module/2-task/index.js
@@ -0,0 +1,31 @@
+
+function print(text) {
+  console.log(text);
+}
+
+
+ function isValid(name) {
+  if (name == null) {
+      return false;
+  } else if(name.includes(" ")) {
+      return false;    
+  } else if(name.length > 4) {
+      return false;
+  } else if(name == "") {
+      return false;
+  }
+  
+  return true;
+}
+
+
+function sayHello() {
+  let userName = prompt('Введите ваше имя');
+
+  if (isValid(userName)) {
+    print(`Welcome back, ${userName}!`);
+  } else {
+    print('Некорректное имя');
+  }
+}
+sayHello();
---
+++ 1-module/3-task/index.js
@@ -0,0 +1,9 @@
+function ucFirst(str) {
+
+  if (!str) return str; 
+      
+      return str[0].toUpperCase() + str.slice(1);
+}
+ucFirst("вася");
+ucFirst('в');
+ucFirst('');
