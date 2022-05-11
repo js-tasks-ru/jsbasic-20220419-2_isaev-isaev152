4e835dab2c01f979732ccb9591bf8fc06457e8f2
Isaev Sergey <86910580+Sergey1522@users.noreply.github.com>
2022-05-11 16:23:47 +0300

    Функция makeFriendsList(friends)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,25 @@
+let friends = [
+  {
+    firstName: 'Artsiom',
+    lastName: 'Mezin',
+  },
+  {
+    firstName: 'Ilia',
+    lastName: 'Kantor',
+  },
+  {
+    firstName: 'Christopher',
+    lastName: 'Michael',
+  },
+];
+
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('UL');
+  for (let item of friends) {
+    const li = document.createElement('LI');
+    li.textContent = `${item.firstName} ${item.lastName}`;
+    ul.append(li);
+  }
+  return ul;
 }
+makeFriendsList(friends);
