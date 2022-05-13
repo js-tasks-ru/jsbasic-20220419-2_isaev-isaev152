9362d75fa398c7aab97d8ae68d69ccd3ed35b3b8
Isaev Sergey <86910580+Sergey1522@users.noreply.github.com>
2022-05-13 16:10:48 +0300

    Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+  const tr = table.querySelectorAll('tr');
+  for (let elem of tr) {
+    for (let item of elem.cells) {
+      if (item.hasAttribute('data-available')) {
+
+        switch (item.dataset.available) {
+          case 'true':
+          elem.classList.add('available');
+          break;
+          case 'false':
+            elem.classList.add('unavailable');
+          break;  
+        }
+          
+      }else elem.setAttribute('hidden', true);
+      
+      switch (item.innerHTML) {
+        case 'm':
+          elem.classList.add('male');
+          break;
+        case 'f':
+          elem.classList.add('female');
+      }
+      if (+item.innerHTML < 18) {
+        elem.style.textDecoration = 'line-through';
+      }
+    }
+  }
 }
