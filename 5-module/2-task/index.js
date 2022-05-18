5505fddc40de50e6c62ea209bf27b757ab481332
Isaev Sergey <86910580+Sergey1522@users.noreply.github.com>
2022-05-18 15:49:16 +0300

    Скрыть элемент по нажатию кнопки

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button');
+  const div = document.querySelector('#text');
+  function handler() {
+    div.hidden = !div.hidden;
+}
+  btn.addEventListener('click', handler);
 }
