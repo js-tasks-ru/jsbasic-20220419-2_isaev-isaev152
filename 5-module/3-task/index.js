2a8dbb8eec35a7de8551809fb3811c5371f0b4ca
Isaev Sergey <86910580+Sergey1522@users.noreply.github.com>
2022-05-18 17:02:58 +0300

    Учебный проект (пролог): Карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  let leftBtn = document.querySelector('.carousel__arrow_left');
+  let rightBtn = document.querySelector('.carousel__arrow_right');
+  let slide = document.querySelector('.carousel__inner');
+  let slideOffset = document.querySelector('.carousel__inner').offsetWidth;
+
+  let position = 0;
+
+  leftBtn.style.display = 'none';
+
+  leftBtn.addEventListener('click', function () {
+    rightBtn.style.display = '';
+    position += slideOffset;
+    console.log(slideOffset);
+    slide.style.transform = `translateX(${position}px)`;
+    if (position == 0) {
+      leftBtn.style.display = 'none';
+    }
+  });
+
+  rightBtn.addEventListener('click', function () {
+    leftBtn.style.display = '';
+    position -= slideOffset;
+    console.log(slideOffset);
+    slide.style.transform = `translateX(${position}px)`;
+    if (position < -2 * slideOffset) {
+      rightBtn.style.display = 'none';
+    }
+  });
 }
